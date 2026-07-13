3ac8fb5d6b9c8fdcf3643953146d7313acf8f8a6
aleks <alekswickey.ost26@gmail.com>
2026-07-13 18:24:39 +0200

    feat: add basic 2D array exercises

--- Arrays/src/main/java/org/enlightenment/Main.java
+++ Arrays/src/main/java/org/enlightenment/Main.java
@@ -1,5 +1,7 @@
 package org.enlightenment;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
     public static void main(String[] args) {
         int[] numbers = new int[6];
@@ -45,8 +47,34 @@ public class Main {
         int[] numbers_4 = {5, 12, 3, 20, 8};
         System.out.println(findMaxElementInArray(numbers_4));
 
-        int[] numbers_5 = {2, 4, 6, 8};
+        int[] numbers_5 = {2, 100, 6, 8, 10};
         System.out.println("Среднее значение " + average(numbers_5));
+        System.out.println("Индекс максимального значения " + findMaxIndex(numbers_5));
+        int[] numbers_6 = {3, 6, 9, 12};
+        increaseArrayNumbersByOne(numbers_6);
+        for (int i = 0; i < numbers_6.length; i++) {
+            System.out.println(numbers_6[i]);
+        }
+        int[] numbers_7 = {5, -2, 8, -7, 10};
+        changeNegativeToZero(numbers_7);
+        for (int i = 0; i < numbers_7.length; i++) {
+            System.out.println(numbers_7[i]);
+        }
+        int[] numbers_8 = {2, 4, 6, 8};
+        multiplyByTwo(numbers_8);
+        for (int i = 0; i < numbers_8.length; i++) {
+            System.out.println(numbers_8[i]);
+        }
+        doubleElements(numbers_8);
+        for (int i = 0; i < numbers_8.length; i++) {
+            System.out.println(numbers_8[i]);
+        }
+
+        int[][] doubleArray = {
+                {1,2,3},
+                {4,5,6}
+        };
+        System.out.println(doubleArray[1][1]);
     }
     public static int sumArray(int[] numbers) {
         int sum = 0;
@@ -115,5 +143,49 @@ public class Main {
             }
         }
         return -1;
+    }
+    public static int findMaxIndex(int[] numbers) {
+        int max = numbers[0];
+        int index = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static int findMinIndex(int[] numbers) {
+        int min = numbers[0];
+        int index = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+   public static void increaseArrayNumbersByOne(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = numbers[i] + 1;
+        }
+    }
+    public static void changeNegativeToZero(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                numbers[i] = 0;
+            }
+        }
+    }
+    public static void multiplyByTwo(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
+        }
+    }
+    public static void doubleElements(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
+        }
     }
 }
